d8ff5f248e912608926d0bea9e6bb63699be6c7d
svtagalog <sam.tags@protonmail.ch>
2024-05-06 04:48:48 +0800

    feat: call screen

--- app/call.js
+++ app/call.js
@@ -1,14 +1,39 @@
-import { Text, StyleSheet, Button, View } from "react-native";
+import { Text, StyleSheet, Button, View, Alert } from "react-native";
 
 import { RTCView } from "react-native-webrtc";
 
 import useCall from "../src/services/hooks/useCall";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import useOnUpdate from "../src/services/hooks/useOnUpdate";
 
 const roomId = "PasaHeRoom";
 
-export default function CallScreen() {
-  const { isMuted, status, userStream, handleToggleMute, streams } =
-    useCall(roomId);
+export default function Call() {
+  const router = useRouter();
+  const params = useLocalSearchParams();
+
+  const callerId = params?.callerId;
+
+  const {
+    isMuted,
+    status,
+    userStream,
+    handleToggleMute,
+    streams,
+    handleCloseMedia,
+  } = useCall(callerId || roomId);
+
+  function handleEndCall() {
+    handleCloseMedia();
+    router.back();
+  }
+
+  useOnUpdate(() => {
+    if (status === "DISCONNECTED") {
+      handleEndCall();
+      Alert.alert("Call Ended", "Call was terminated by the receiver.");
+    }
+  }, [status]);
 
   return (
     <>
@@ -16,7 +41,7 @@ export default function CallScreen() {
         <Text>{status}</Text>
       </View>
 
-      <Button color="#ef4444" title="End call" onPress={() => {}} />
+      <Button color="#ef4444" title="End call" onPress={handleEndCall} />
 
       {userStream && (
         <Button
