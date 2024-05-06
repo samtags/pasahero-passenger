7cd52d5336b36d1d67bcde4a04a82f626bfd1bfa
svtagalog <sam.tags@protonmail.ch>
2024-05-07 02:12:06 +0800

    feat: call stable version

--- app/call/call.js
+++ app/call/call.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Text, StyleSheet, Button, View, Alert } from "react-native";
+import { StyleSheet, Button } from "react-native";
 
 import {
   RTCPeerConnection,
@@ -11,6 +11,7 @@ import {
 import firebase from "@react-native-firebase/app";
 import "@react-native-firebase/firestore";
 import { useRouter } from "expo-router";
+import useOnUpdate from "../../src/services/hooks/useOnUpdate";
 
 const app = firebase.app();
 db = app.firestore();
@@ -26,164 +27,32 @@ const configuration = {
 
 const roomId = "PasaHeRoom";
 
-const subscriptions = [];
-
-const pendingPeerCandidates = [];
-let remoteDescriptionReady = false;
-
 export default function CallScreen() {
   const router = useRouter();
 
-  const peerConnectionRef = useRef(null);
-  const [localStream, setLocalStream] = useState();
-  const [streams, setStreams] = useState([]);
+  const { status, isMuted, handleToggleMute, userStream, streams } =
+    useDial(roomId);
 
-  const [isMuted, setIsMuted] = useState(false);
-
-  useEffect(() => {
-    startCall(roomId);
-
-    return handleCleanUp;
-  }, []);
-
-  function handleUnsubscribe() {
-    subscriptions.forEach((unsbcrb) => unsbcrb());
-  }
-
-  function handleCleanUp() {
-    handleCloseMedia();
-    handleUnsubscribe();
-  }
-
-  function handleCloseMedia() {
-    if (peerConnectionRef.current) {
-      peerConnectionRef.current.getTransceivers().forEach((transceiver) => {
-        transceiver.stop();
-      });
-      peerConnectionRef.current.close();
+  useOnUpdate(() => {
+    if (status === "TERMINATED") {
+      handleEndCall();
     }
-
-    streams?.forEach((stream) => {
-      stream.getTracks().forEach((track) => track.stop());
-    });
-  }
-
-  async function handleRemoveRoom(id) {
-    await db.collection("rooms").doc(id).delete();
-  }
+  }, [status]);
 
   function handleEndCall() {
-    handleCloseMedia();
-    setLocalStream();
-    handleRemoveRoom(roomId);
+    db.collection("rooms").doc(roomId).delete();
     router.back();
   }
-
-  const startCall = async (id) => {
-    // delete room if existing
-    await handleRemoveRoom(id);
-
-    const newStream = await mediaDevices.getUserMedia({
-      audio: true,
-    });
-
-    setLocalStream(newStream);
-
-    const peerConnection = new RTCPeerConnection(configuration);
-
-    // Add each track from the localStream to the RTCPeerConnection
-    newStream.getTracks().forEach((track) => {
-      peerConnection.addTrack(track, newStream);
-    });
-
-    const roomRef = await db.collection("rooms").doc(id);
-    const callerCandidatesCollection = roomRef.collection("callerCandidates");
-
-    peerConnection.onicecandidate = (e) => {
-      if (!e?.candidate) return;
-      callerCandidatesCollection.add(e.candidate.toJSON());
-    };
-
-    peerConnection.oniceconnectionstatechange = (e) => {
-      if (peerConnection?.iceConnectionState == "disconnected") {
-        Alert.alert("Call ended", "Call receiver ended the call.");
-        handleEndCall();
-      }
-
-      if (peerConnection?.iceConnectionState == "connected") {
-        // other party is connected
-        Alert.alert("Answered", "Call receiver answered your call!");
-      }
-    };
-
-    peerConnection.ontrack = (e) => {
-      if (e?.streams) setStreams(e?.streams);
-    };
-
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-
-    const roomWithOffer = { offer };
-    await roomRef.set(roomWithOffer);
-
-    const unsubs = roomRef.onSnapshot(async (snapshot) => {
-      const data = snapshot.data();
-      if (!peerConnection.currentRemoteDescription && data?.answer) {
-        const rtcSessionDescription = new RTCSessionDescription(data?.answer);
-        await peerConnection.setRemoteDescription(rtcSessionDescription);
-        remoteDescriptionReady = true;
-
-        // add the candidates when desciption is ready.
-        if (pendingPeerCandidates.length) {
-          pendingPeerCandidates.forEach((candidate) => {
-            peerConnection.addIceCandidate(candidate);
-          });
-        }
-      }
-    });
-
-    if (unsubs) subscriptions.push(unsubs);
-
-    const unsubscribe = roomRef
-      .collection("calleeCandidates")
-      .onSnapshot((snapshot) => {
-        snapshot.docChanges().forEach(async (change) => {
-          if (change.type === "added") {
-            let data = change.doc.data();
-
-            // wait for the candidate description
-            if (remoteDescriptionReady) {
-              peerConnection.addIceCandidate(new RTCIceCandidate(data));
-            } else {
-              pendingPeerCandidates.push(new RTCIceCandidate(data));
-            }
-          }
-        });
-      });
-
-    if (unsubscribe) subscriptions.push();
-
-    peerConnectionRef.current = peerConnection;
-  };
-
-  const toggleMute = () => {
-    console.log(localStream.getAudioTracks());
-
-    localStream.getAudioTracks().forEach((track) => {
-      track.enabled = !track.enabled;
-      setIsMuted(!track.enabled);
-    });
-  };
 
   return (
     <>
       <Button color="#ef4444" title="End call" onPress={handleEndCall} />
 
-      {localStream && (
+      {userStream && (
         <Button
           color="#a3a3a3"
           title={isMuted ? "Unmute" : "Mute"}
-          onPress={toggleMute}
+          onPress={handleToggleMute}
         />
       )}
 
@@ -199,34 +68,174 @@ export default function CallScreen() {
 }
 
 const styles = StyleSheet.create({
-  heading: {
-    alignSelf: "center",
-    fontSize: 30,
-  },
-  rtcview: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "black",
-    margin: 5,
-  },
   rtc: {
     flex: 1,
     width: "100%",
     height: "100%",
   },
-  toggleButtons: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  callButtons: {
-    padding: 10,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  buttonContainer: {
-    margin: 5,
-  },
 });
+
+function useDial(roomId) {
+  const peerConnectionRef = useRef(null);
+  const [userStream, setUserStream] = useState();
+  const [streams, setStreams] = useState([]);
+  const [isMuted, setIsMuted] = useState(false);
+  const [status, setStatus] = useState("CONNECTING");
+
+  useEffect(() => {
+    const subscriptions = [];
+    const pendingPeerCandidates = [];
+    let remoteDescriptionReady = false;
+
+    (async () => {
+      const roomRef = await db.collection("rooms").doc(roomId);
+      const callerCandidatesCollection = roomRef.collection("callerCandidates");
+
+      // clean up the room
+      await db.collection("rooms").doc(roomId).delete();
+
+      // create stream
+      const userStream = await mediaDevices.getUserMedia({
+        audio: true,
+      });
+
+      const peerConnection = new RTCPeerConnection(configuration);
+
+      userStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, userStream);
+      });
+
+      peerConnection.onicecandidate = (e) => {
+        if (!e?.candidate) return;
+        callerCandidatesCollection.add(e.candidate.toJSON());
+      };
+
+      peerConnection.oniceconnectionstatechange = (e) => {
+        if (peerConnection?.iceConnectionState == "disconnected") {
+          // Alert.alert("Call ended", "Call receiver ended the call.");
+          setStatus("TERMINATED");
+          handleCloseMedia();
+        }
+
+        if (peerConnection?.iceConnectionState == "connected") {
+          // other party is connected
+          // Alert.alert("Answered", "Call receiver answered your call!");
+          setStatus("CONNECTED");
+        }
+      };
+
+      peerConnection.ontrack = (e) => {
+        if (e?.streams) setStreams(e?.streams);
+      };
+
+      // todo: maybe move after the peer connection is made
+      // make sure all media are attached before creating a offer
+      // https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createOffer
+      const offer = await peerConnection.createOffer();
+      await peerConnection.setLocalDescription(offer);
+
+      const roomWithOffer = { offer };
+      await roomRef.set(roomWithOffer);
+
+      subscriptions.push(
+        roomRef.onSnapshot(async (snapshot) => {
+          const data = snapshot.data();
+          if (!peerConnection.currentRemoteDescription && data?.answer) {
+            const rtcSessionDescription = new RTCSessionDescription(
+              data?.answer
+            );
+            await peerConnection.setRemoteDescription(rtcSessionDescription);
+            remoteDescriptionReady = true;
+
+            // add the candidates when desciption is ready.
+            if (pendingPeerCandidates.length) {
+              pendingPeerCandidates.forEach((candidate) => {
+                peerConnection.addIceCandidate(candidate);
+              });
+            }
+          }
+        })
+      );
+
+      subscriptions.push(
+        // todo: reuse roomRef with other document operations
+        roomRef.collection("calleeCandidates").onSnapshot((snapshot) => {
+          snapshot.docChanges().forEach(async (change) => {
+            if (change.type === "added") {
+              let data = change.doc.data();
+
+              // wait for the candidate description
+              if (remoteDescriptionReady) {
+                peerConnection.addIceCandidate(new RTCIceCandidate(data));
+              } else {
+                pendingPeerCandidates.push(new RTCIceCandidate(data));
+              }
+            }
+          });
+        })
+      );
+
+      peerConnectionRef.current = peerConnection;
+      setUserStream(userStream);
+    })();
+
+    return () => {
+      // close the rtc connection
+      if (peerConnectionRef.current) {
+        peerConnectionRef.current.getTransceivers().forEach((transceiver) => {
+          transceiver.stop();
+        });
+        peerConnectionRef.current.close();
+      }
+
+      // handle close media
+      streams?.forEach((stream) => {
+        stream.getTracks().forEach((track) => track.stop());
+      });
+
+      // handle unsubscribe to snapshot listeners
+      subscriptions.forEach((unsubscribe) => unsubscribe());
+    };
+    // return handleCleanUp;
+  }, []);
+
+  // stream clean up
+  useEffect(() => {
+    if (streams.length) {
+      return () => {
+        streams?.forEach((stream) => {
+          stream.getTracks().forEach((track) => track.stop());
+        });
+      };
+    }
+  }, [streams]);
+
+  function handleToggleMute() {
+    userStream.getAudioTracks().forEach((track) => {
+      track.enabled = !track.enabled;
+      setIsMuted(!track.enabled);
+    });
+  }
+
+  function handleCloseMedia() {
+    if (peerConnectionRef.current) {
+      peerConnectionRef.current.getTransceivers().forEach((transceiver) => {
+        transceiver.stop();
+      });
+      peerConnectionRef.current.close();
+    }
+  }
+
+  function handleHangup() {
+    //
+  }
+
+  return {
+    userStream,
+    streams,
+    isMuted,
+    handleToggleMute,
+    peerConnectionRef,
+    status,
+  };
+}
