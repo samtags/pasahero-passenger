7999fd509a972ba6759e9e238210630b0c9d8595
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-05-13 00:41:16 +0800

    feat: home screen

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,8 +1,9 @@
 import { useMMKVString } from "react-native-mmkv";
 import { Stack, useRouter } from "expo-router";
-import { View, SafeAreaView, StyleSheet } from "react-native";
+import { View, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
 import Text from "../../components/text";
 import Mapbox from "@rnmapbox/maps";
+import { Image } from "expo-image";
 
 export default function Home() {
   const router = useRouter();
@@ -17,15 +18,35 @@ export default function Home() {
         }}
       />
       <SafeAreaView style={styles.full}>
-        <View style={styles.absolute}>
+        <View pointerEvents="box-none" style={styles.absolute}>
           <View style={styles.content}>
             <Text size={18} weight="bold" color="#757477">
-              Hey,
+              Hi,
             </Text>
             <Text size={28} weight="bold" color="#353579">
               Where are we going?
             </Text>
             <View style={{ marginTop: 16 }} />
+            <TouchableOpacity>
+              <View
+                style={{
+                  backgroundColor: "#F0F0F0",
+                  paddingHorizontal: 16,
+                  paddingVertical: 10.5,
+                  flexDirection: "row",
+                  borderRadius: 10,
+                  alignItems: "center",
+                  gap: 8,
+                }}
+              >
+                <Image
+                  style={{ width: 12, height: 12, marginTop: 4 }}
+                  cachePolicy="memory-disk"
+                  source="https://firebasestorage.googleapis.com/v0/b/pasahero-5c989.appspot.com/o/com.pasahero.passenger%2FDestination.png?alt=media&token=e92cc2d1-77c3-486f-9793-3c0827ca5aef"
+                />
+                <Text color="#B9B8BB">Going to?</Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
         <Mapbox.MapView
@@ -64,8 +85,7 @@ const styles = StyleSheet.create({
   content: {
     backgroundColor: "white",
     paddingHorizontal: 18,
-    paddingBottom: 18,
-    paddingTop: 32,
+    paddingVertical: 32,
     borderTopLeftRadius: 34,
     borderTopRightRadius: 34,
   },
