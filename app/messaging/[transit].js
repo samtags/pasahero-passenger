2ffd052e3d319b5c63b315f600025bd02ba8e0b7
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-06-22 04:21:02 +0800

    chore: prevent back functionality

--- app/messaging/[transit].js
+++ app/messaging/[transit].js
@@ -1,14 +1,42 @@
-import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import {
+  Stack,
+  useLocalSearchParams,
+  useNavigation,
+  useRouter,
+} from "expo-router";
 import Text from "../../src/components/text";
 import Messaging from "../../src/screens/messaging/[transit]";
 import { TouchableOpacity, View } from "react-native";
 import { Image } from "expo-image";
 import useGetDriver from "../../src/services/queries/useGetDriver";
+import { useEffect } from "react";
 
 export default function Entry(props) {
   const router = useRouter();
   const params = useLocalSearchParams();
   const { data: driver } = useGetDriver(params?.driver_id);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("beforeRemove", (e) => {
+      // e.preventDefault(); // Prevent default action (navigation back)
+      // redirect to the match request screen
+      // Saving this for future cases.
+      // Custom logic before going back
+      // Alert.alert("Confirm exit", "Do you want to go back?", [
+      //   { text: "Cancel", style: "cancel", onPress: () => {} },
+      //   {
+      //     text: "Yes",
+      //     style: "destructive",
+      //     onPress: () => navigation.dispatch(e.data.action),
+      //   },
+      // ]);
+    });
+
+    // Clean up the event listener on unmount
+    return unsubscribe;
+  }, [navigation]);
 
   function handlePressCall() {
     router.navigate({
