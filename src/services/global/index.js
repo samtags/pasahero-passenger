3eb68d44feb6485fb871e93b0174ae21bf2d177f
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-06-29 21:58:54 +0800

    chore: message

--- src/services/global/index.js
+++ src/services/global/index.js
@@ -2,14 +2,14 @@ import log from "../log";
 
 export const UNSAFE_registerProperty = (key, value) => {
   if (global[key]) {
-    log.debug("Overwriting existing property. [PROPERTY_NAME]:", key);
+    log.debug(`Overwriting existing property. [PROPERTY_NAME]: ${key}`);
   }
   global[key] = value;
 };
 
 export const UNSAFE_registerMethod = (key, handler) => {
   if (global[key]) {
-    log.debug("Overwriting existing method. [METHOD_NAME]:", key);
+    log.debug(`Overwriting existing method. [METHOD_NAME]: ${key}`);
   }
   global[key] = handler;
 };
