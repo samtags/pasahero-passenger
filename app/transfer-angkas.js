c90fdc9b73ac675c923bbf71a62b26db1a8f986d
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-07-03 03:41:59 +0800

    feat: trip transfer webview

---
+++ app/transfer-angkas.js
@@ -0,0 +1,64 @@
+import { Stack } from "expo-router";
+import { WebView } from "react-native-webview";
+import { StyleSheet, View } from "react-native";
+import Text from "../src/components/text";
+import { useState } from "react";
+import Optional from "../src/components/optional";
+import LottieView from "lottie-react-native";
+
+export default function Entry(props) {
+  const [showWebview, setShowWebView] = useState(false);
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerTitle: () => (
+            <Text size={19} weight="bold" color="#353579">
+              Transfer to JoyRide
+            </Text>
+          ),
+          headerTitleAlign: "center",
+          headerTintColor: "#757477",
+          animation: "ios",
+        }}
+      />
+
+      <View style={{ backgroundColor: "white", height: "100%" }}>
+        <Optional condition={showWebview === false}>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <LottieView
+              autoPlay
+              loop
+              style={{
+                width: 220,
+                height: 220,
+                marginTop: -64,
+                marginBottom: -88,
+              }}
+              source={require("../src/assets/json/autocomplete-preloader.json")}
+            />
+          </View>
+        </Optional>
+        <WebView
+          onLoadEnd={() => {
+            setTimeout(() => {
+              setShowWebView(true);
+            }, 4000);
+          }}
+          style={[styles.container, { opacity: showWebview ? 1 : 0 }]}
+          source={{
+            uri: `https://pasahero.notion.site/Transfer-trip-to-Angkas-7e46bd53fece48589422e8888d3fca26`,
+          }}
+        />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: -60,
+  },
+});
