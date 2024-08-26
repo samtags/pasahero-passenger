8daa1932e4889a10e351c909ce8b43dcedf57001
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-08-26 21:47:44 +0800

    feat: integrate in-house logging service

--- src/services/log/index.js
+++ src/services/log/index.js
@@ -9,15 +9,25 @@ const options = {
 };
 
 function log(message, payload = {}) {
-  payload.level = payload.level || "debug";
   payload.entity = payload.entity || {};
   payload.entity.name = "pasahero.passenger";
+
+  // implementing log structure
+  // https://www.notion.so/pasahero/Log-coherence-83cf75228e014d5289e757b40a4697bc?pvs=4
+
+  payload.level = payload.level || "debug";
   payload.message = payload?.message || message;
+  payload.created_at = new Date().toISOString();
+  payload.context = payload.context || {};
+  payload.context["@service_name"] = "pasahero.passenger";
 
   const body = JSON.stringify(payload);
   options.body = body;
 
+  // todo: remove new relic once we have centralized the logging service
   fetch("https://log-api.newrelic.com/log/v1", options);
+  fetch("https://asia-southeast2-pasahero-5c989.cloudfunctions.net/log/", options); // prettier-ignore
+
   console[payload.level](message, payload);
 }
 
