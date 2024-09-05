0f84b406f7a5c017b4f7c8ccbf9fe7fe9ca31269
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-09-06 00:55:55 +0800

    chore: change version to 1.0.7

--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -64,7 +64,7 @@ export default function Account() {
         </View>
       </ScrollView>
       <View style={{ padding: 24 }}>
-        <Text color="#bbbbbb">Beta 1.0.6</Text>
+        <Text color="#bbbbbb">1.0.7-beta</Text>
       </View>
     </View>
   );
