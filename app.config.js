4096558b8a96df3ef4fb5035bcb5a636b1a3bfe8
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-09-24 03:43:55 +0800

    chore: increment version

--- app.config.js
+++ app.config.js
@@ -13,7 +13,7 @@ module.exports = ({ config }) => {
     updates: {
       url: "https://u.expo.dev/2edea32b-6502-426d-bbd6-bfbe43e700a5",
     },
-    runtimeVersion: "1.0.9",
+    runtimeVersion: "1.0.10",
     plugins: [
       "expo-router",
       [
