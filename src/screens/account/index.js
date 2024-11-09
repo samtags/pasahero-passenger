48ca8412fb9663ee3f04cc4b88193f4902e7db81
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2024-11-09 15:48:13 +0800

    chore: update version

--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -66,7 +66,7 @@ export default function Account() {
         </View>
       </ScrollView>
       <View style={{ padding: 24 }}>
-        <Text color="#bbbbbb">1.0.11-beta</Text>
+        <Text color="#bbbbbb">1.0.12-beta</Text>
       </View>
     </View>
   );
